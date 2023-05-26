6cc7081046617b3818c4c2cf2d225577ff4ab5bf
Thamiris Borges <87441572+Thami03@users.noreply.github.com>
2023-05-26 16:16:18 -0300

    atualizando

--- cinespace/lib/cadastrar.dart
+++ cinespace/lib/cadastrar.dart
@@ -14,7 +14,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(title: 'Cinespace'),
+      home: const MyHomePage(title: 'Cadastrar nova conta'),
     );
   }
 }
