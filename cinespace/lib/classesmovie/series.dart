4b740e4969816b2d493bcf78d3f63e595587a9fa
FelipeSouza14 <felipesouzabenicio@gmail.com>
2023-05-31 16:18:01 -0300

    Finalizando a parte de series da tela principal

--- cinespace/lib/classesmovie/series.dart
+++ cinespace/lib/classesmovie/series.dart
@@ -2,11 +2,10 @@ import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:transparent_image/transparent_image.dart';
 
+class BestSeries extends StatelessWidget {
+  final List tvbest;
 
-class PopularMovie extends StatelessWidget {
-  final List popular;
-
-  const PopularMovie({Key? key, required this.popular}) : super(key: key);
+  const BestSeries({Key? key, required this.tvbest}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
@@ -15,43 +14,38 @@ class PopularMovie extends StatelessWidget {
       child: Column(
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
-          Text('Filmes mais populares',
+          Text('Melhores séries para assistir',
               style: GoogleFonts.breeSerif(fontSize: 25)),
-          
           Container(
             height: 270,
             child: ListView.builder(
-              itemCount: popular.length,
+              itemCount: tvbest.length,
               scrollDirection: Axis.horizontal,
               itemBuilder: (context, index) {
                 return InkWell(
-                  onDoubleTap: () {
-
-
-                  },
-
+                  onDoubleTap: () {},
                   child: Container(
                     padding: EdgeInsets.all(5),
                     width: 140,
                     child: Column(
                       children: [
                         Container(
-                                child: ClipRRect(
-                                child: FadeInImage.memoryNetwork(
-                                  placeholder: kTransparentImage, 
-                                  image: 'https://image.tmdb.org/t/p/w500'+popular[index]['poster_path']),
-                                  borderRadius: BorderRadius.circular(10),
-                                  )
-                                
-                              
-                        ),
+                            child: ClipRRect(
+                          child: FadeInImage.memoryNetwork(
+                              placeholder: kTransparentImage,
+                              image: 'https://image.tmdb.org/t/p/w500' +
+                                  tvbest[index]['poster_path']),
+                          borderRadius: BorderRadius.circular(10),
+                        )),
                         Container(
-                          child: Text(popular[index]['title']!= null? popular[index]['title']:'carregando...'),
+                          child: Text(tvbest[index]['title'] != null
+                              ? tvbest[index]['title']
+                              : 'carregando...'),
                         )
                       ],
                     ),
                   ),
-                ) ;
+                );
               },
             ),
           )
