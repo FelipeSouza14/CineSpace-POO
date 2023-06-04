655b77cacb7168d7f1f77a814a373ba0ba64c1cf
Thamiris Borges <87441572+Thami03@users.noreply.github.com>
2023-06-03 21:31:48 -0300

    Adicionando tela de cadastro

---
+++ cinespace/lib/pages/cadastro_page.dart
@@ -0,0 +1,84 @@
+import 'package:flutter/material.dart';
+
+class CadastroPage extends StatefulWidget {
+  @override
+  _CadastroPageState createState() => _CadastroPageState();
+}
+
+class _CadastroPageState extends State<CadastroPage> {
+  String email = '';
+  String senha = '';
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SingleChildScrollView(
+        child: SizedBox(
+          width: MediaQuery.of(context).size.width,
+          height: MediaQuery.of(context).size.height,
+          child: Padding(
+            padding: const EdgeInsets.all(100.0),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                TextField(
+                  onChanged: (text) {
+                    email = text;
+                  },
+                  decoration: InputDecoration(
+                    labelText: 'Nome',
+                    border: OutlineInputBorder(),
+                  ),
+                ),
+                SizedBox(height: 10),
+                TextField(
+                  onChanged: (text) {
+                    senha = text;
+                  },
+                  decoration: InputDecoration(
+                    labelText: 'Email',
+                    border: OutlineInputBorder(),
+                  ),
+                ),
+                SizedBox(height: 10),
+                TextField(
+                  onChanged: (text) {
+                    email = text;
+                  },
+                  decoration: InputDecoration(
+                    labelText: 'Telefone',
+                    border: OutlineInputBorder(),
+                  ),
+                ),
+                SizedBox(height: 10),
+                TextField(
+                  onChanged: (text) {
+                    senha = text;
+                  },
+                  obscureText: true,
+                  decoration: InputDecoration(
+                    labelText: 'Senha',
+                    border: OutlineInputBorder(),
+                  ),
+                ),
+                SizedBox(height: 10),
+                ElevatedButton(
+                  onPressed: () {
+                    print("salvado");
+                  },
+                  child: Text('Salvar'),
+                ),
+                SizedBox(height: 10),
+                ElevatedButton(
+                  onPressed: () {
+                    Navigator.of(context).pushReplacementNamed('/');
+                  },
+                  child: Text('Voltar'),
+                )
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
