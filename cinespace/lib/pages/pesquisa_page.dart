e678a9416a3efce8232d5b25760ba01288a900b2
Thamiris Borges <87441572+Thami03@users.noreply.github.com>
2023-06-09 23:40:58 -0300

    ajeitando busca

--- cinespace/lib/pages/pesquisa_page.dart
+++ cinespace/lib/pages/pesquisa_page.dart
@@ -1,9 +1,12 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 
 class PesquisaPage extends SearchDelegate<String> {
   @override
   String get searchFieldLabel => 'Procurar filmes/series';
+
   @override
   List<Widget>? buildActions(BuildContext context) {
     // TODO: implement buildActions
@@ -37,5 +40,17 @@ class PesquisaPage extends SearchDelegate<String> {
   Widget buildSuggestions(BuildContext context) {
     // TODO: implement buildSuggestions
     return Container();
+  }
+
+  Future<List> sugestoes() async {
+    var url = Uri.parse('htts://www.themoviedb.org/movie?search=$query');
+    Map<String, String> headers = {
+      'Authorization': '1d9a3b2956d60bfebc1a8a6420c20bb0',
+    };
+    var response = await http.get(url, headers: headers);
+    if (response.statusCode == 200) {
+      return json.decode(response.body).map((escolha) => escolha).toList();
+    }
+    throw Exception("erro");
   }
 }
